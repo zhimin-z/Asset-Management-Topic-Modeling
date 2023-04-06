0a4dbeb6d2c8ba1f0419de4b14ff084249bb12d2
zhimin-z <knightyzhao@gmail.com>
2023-04-06 18:07:09 +0000

    scrub

--- Code/best_model.py
+++ Code/best_model.py
@@ -174,8 +174,8 @@ umap_model = UMAP(n_components=config_defaults['n_components'],
                   metric=config_defaults['metric_distane'], low_memory=config_defaults['low_memory'])
 
 # Step 3 - Cluster reduced embeddings
-samples = int(config_challenge['min_cluster_size']
-                  * config_challenge['min_samples_pct'])
+samples = int(config_solution['min_cluster_size']
+                  * config_solution['min_samples_pct'])
 samples = samples if samples > config_defaults['min_samples'] else config_defaults['min_samples']
 hdbscan_model = HDBSCAN(
     min_cluster_size=config_solution['min_cluster_size'], min_samples=min_samples, prediction_data=True)
