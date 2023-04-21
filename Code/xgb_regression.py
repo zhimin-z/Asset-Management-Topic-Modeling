26222f15ba7e7345747b197cdf2a05dccbc2e662
zhimin-z <knightyzhao@gmail.com>
2023-04-20 20:16:56 -0400

    scrub

--- Code/xgb_regression.py
+++ Code/xgb_regression.py
@@ -5,8 +5,6 @@ import pandas as pd
 import xgboost as xgb
 from sklearn.model_selection import cross_val_score
 
-wandb_project = 'challenge-solved-time-regression-modeling'
-
 os.environ["WANDB_API_KEY"] = '9963fa73f81aa361bdbaf545857e1230fc74094c'
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
 os.environ["WANDB__SERVICE_WAIT"] = "100"
@@ -18,7 +16,7 @@ config_defaults = {
 }
 
 config_sweep = {
-    "name": "XGB Regression",
+    "name": "XGB Regression (adjusted)",
     "method": "bayes",
     "metric": {
         'name': 'root mean square log error',
@@ -38,21 +36,32 @@ path_dataset = 'Dataset'
 path_result = 'Result'
 path_general = os.path.join(path_result, 'General')
 
-df = pd.read_json(os.path.join(path_general, 'solved_imputed_original.json'))
-
-X = df[['Challenge_answer_count', 'Challenge_comment_count', 'Challenge_participation_count', 'Challenge_information_entropy', 'Challenge_link_count', 'Challenge_readability', 'Challenge_score', 'Challenge_sentence_count', 'Challenge_unique_word_count',
-        'Challenge_view_count', 'Challenge_word_count', 'Solution_comment_count', 'Solution_information_entropy', 'Solution_link_count', 'Solution_readability', 'Solution_score', 'Solution_sentence_count', 'Solution_unique_word_count', 'Solution_word_count']]
-y = df['Challenge_solved_time']
+count = 500
+wandb_project = 'challenge-solved-time-regression-modeling'
+wandb.login()
 
 
 def _train():
     with wandb.init() as run:
         run.config.setdefaults(config_defaults)
-        regressor = xgb.XGBRegressor(objective=run.config.objective, max_depth=run.config.max_depth, n_estimators=wandb.config.n_estimators, eta=wandb.config.eta)
+        regressor = xgb.XGBRegressor(objective=run.config.objective, max_depth=run.config.max_depth,
+                                     n_estimators=wandb.config.n_estimators, eta=wandb.config.eta)
         scores = cross_val_score(regressor, X, y, cv=run.config.cv)
         wandb.log({'root mean square log error': scores.mean()})
 
 
-wandb.login()
+config_sweep['name'] = 'XGB Regression (original)'
+df = pd.read_json(os.path.join(path_general, 'solved_imputed_original.json'))
+X = df[['Challenge_answer_count', 'Challenge_comment_count', 'Challenge_participation_count', 'Challenge_information_entropy', 'Challenge_link_count', 'Challenge_readability', 'Challenge_score', 'Challenge_sentence_count', 'Challenge_unique_word_count',
+        'Challenge_view_count', 'Challenge_word_count', 'Solution_comment_count', 'Solution_information_entropy', 'Solution_link_count', 'Solution_readability', 'Solution_score', 'Solution_sentence_count', 'Solution_unique_word_count', 'Solution_word_count']]
+y = df['Challenge_solved_time']
 sweep_id = wandb.sweep(config_sweep, project=wandb_project)
-wandb.agent(sweep_id, function=_train, count=500)
+wandb.agent(sweep_id, function=_train, count=count)
+
+config_sweep['name'] = 'XGB Regression (adjusted)'
+df = pd.read_json(os.path.join(path_general, 'solved_imputed_adjusted.json'))
+X = df[['Challenge_answer_count', 'Challenge_comment_count', 'Challenge_participation_count', 'Challenge_information_entropy', 'Challenge_link_count', 'Challenge_readability', 'Challenge_score', 'Challenge_sentence_count', 'Challenge_unique_word_count',
+        'Challenge_view_count', 'Challenge_word_count', 'Solution_comment_count', 'Solution_information_entropy', 'Solution_link_count', 'Solution_readability', 'Solution_score', 'Solution_sentence_count', 'Solution_unique_word_count', 'Solution_word_count']]
+y = df['Challenge_adjusted_solved_time']
+sweep_id = wandb.sweep(config_sweep, project=wandb_project)
+wandb.agent(sweep_id, function=_train, count=count)
