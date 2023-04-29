b0148b7e8d9f02101c96e970bf527860162b51dd
zhimin-z <knightyzhao@gmail.com>
2023-04-29 18:19:37 -0400

    remove heatmap, redo feature importance, add xgboost classifier, remove hyperparamer tuning

--- Code/str_1.py
+++
@@ -1,4 +0,0 @@
-from solved_time_regression import XGBRegression
-
-xgb_regression = XGBRegression(adjusted=True)
-xgb_regression.sweep()
--- Code/str_2.py
+++
@@ -1,4 +0,0 @@
-from solved_time_regression import XGBRegression
-
-xgb_regression = XGBRegression(adjusted=False)
-xgb_regression.sweep()
