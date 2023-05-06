5256fcd8cdcaf9763c06e8a08c7b61012cae2c59
JIMMY ZHAO <knightyzhao@gmail.com>
2023-05-05 20:47:31 -0400

    scrub

--- Code/best_model.py
+++ Code/best_model.py
@@ -194,5 +194,5 @@ del df['Solution_original_content']
 del df['Solution_preprocessed_content']
 del df['Solution_gpt_summary']
 
-df.to_json(os.path.join(path_general, 'original.json'),
+df.to_json(os.path.join(path_general, 'topics.json'),
            indent=4, orient='records')
