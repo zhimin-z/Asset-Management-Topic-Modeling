3fbfeb39fa4b1d4e6a73b059e7c09b73a9ac3f61
zhimin-z <knightyzhao@gmail.com>
2023-07-23 07:13:56 +0000

    scrub

--- Code/RQ1/Special Topic Modeling/best_model.py
+++ Code/RQ1/Special Topic Modeling/best_model.py
@@ -11,8 +11,8 @@ path_model = os.path.join(path_topic, 'Model')
 path_root_cause = path_anomaly = os.path.join(path_topic, 'Anomaly')
 path_solution = os.path.join(path_topic, 'Solution')
 
-name_model_root_cause = name_model_anomaly = 'anomaly_1e48u4hl'
-name_model_solution = 'solution_wxypawnp'
+name_model_root_cause = name_model_anomaly = 'anomaly_r7npnwek'
+name_model_solution = 'solution_bzy0vzyz'
 
 df = pd.read_json(os.path.join(path_output, 'labels.json'))
 
