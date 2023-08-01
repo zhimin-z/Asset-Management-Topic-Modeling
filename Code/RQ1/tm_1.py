2fdf6dd232166190ea9cc8390e9a44fd9ef603cb
zhimin-z <knightyzhao@gmail.com>
2023-08-01 23:33:34 +0000

    scrub

--- Code/RQ1/tm_1.py
+++ Code/RQ1/tm_1.py
@@ -1,4 +1,4 @@
 from topic_modeling import TopicModeling
 
-topic_model = TopicModeling('Challenge_original_content')
+topic_model = TopicModeling('Challenge_preprocessed_title')
 topic_model.sweep()
