8057b38c5769ff77fb4b83fb3a8961869d47fa4b
zhimin-z <knightyzhao@gmail.com>
2023-08-22 07:14:07 +0000

    reorgnize file strcu

--- Code/RQ3/tm_1.py
+++
@@ -1,4 +0,0 @@
-from topic_modeling import TopicModeling
-
-topic_model = TopicModeling('Challenge_summary')
-topic_model.sweep()
--- Code/RQ3/tm_2.py
+++
@@ -1,4 +0,0 @@
-from topic_modeling import TopicModeling
-
-topic_model = TopicModeling('Challenge_root_cause_summary', min_cluster_size=20)
-topic_model.sweep()
