7e173326ba9ec136af30047780b53387478d3cd8
zhimin-z <knightyzhao@gmail.com>
2023-08-25 04:51:40 +0000

    scrub

--- Code/RQ3/best_model.py
+++ Code/RQ3/best_model.py
@@ -7,7 +7,7 @@ from sentence_transformers import SentenceTransformer
 
 path_output = os.path.join(os.getcwd(), 'Result', 'RQ3')
 path_model = os.path.join(path_output, 'Model')
-model_output = 'Resolution_summary_deufr20x'
+model_output = 'Resolution_summary_rss7vg5t'
 
 embedding_model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
 df = pd.read_json(os.path.join(path_output, 'labels.json'))
